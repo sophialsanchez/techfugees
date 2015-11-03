33e5e342ceaf027f5f0b7624db51acac9b0ee877
Sophia Sanchez <sophiasanchez@sparkcentral.com>
2015-11-02 20:27:37 -0800

    update graph search

--- techfugees_google_sheets_analysis.py
+++ techfugees_google_sheets_analysis.py
@@ -4,26 +4,34 @@
 # Also, the json file with all the fun Google authorization passwords you need
 # is not shared in this repo.
 
+
+# To Dos: make code faster, add checks for missing or corrupt data, update which sheet it points to (not the correct one now)
+
+import csv
 import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
+import pandas
 
-json_key = json.load(open('techfugees-41d6a19e279a.json'))
-scope = ['https://spreadsheets.google.com/feeds']
+#json_key = json.load(open('techfugees-41d6a19e279a.json'))
+#scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-data = gspread.authorize(credentials)
-wks = data.open("GITOC Migrant Leg Prices Database 1 October 2015.xlsx").sheet1
+#credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+#data = gspread.authorize(credentials)
+#wks = data.open("GITOC Migrant Leg Prices Database 1 October 2015.xlsx")
+#wks = wks.get_worksheet(5)
+wks = pandas.read_csv('Prices.csv', header=1)
 
 def listAllCountryPaths(startCountry, endCountry):
-	paths = findCountryPathRecursion(startCountry, endCountry, 0)
-
-	if len(paths) == 0:
+	paths = findCountryPathRecursion(startCountry, endCountry)
+	if startCountry == endCountry:
+		print("The start and end countries must be different.")
+	elif len(paths) == 0:
 		print("We're sorry! There are no matching routes in our database for a trip from %s to %s." % (startCountry, endCountry))
 	elif len(paths) == 1:
 		print("There is 1 trip in our database from %s to %s. The path is:" % (startCountry, endCountry))
-		print(path[0::2]),
-		price = sum(path[1::2])
+		print(paths[0][0::2]),
+		price = int(paths[0][1])
 		print("Trip price: %d" % price)
 	else:
 		print("There are %d trips in our database from %s to %s. The paths are:" % (len(paths), startCountry, endCountry))
@@ -32,29 +40,28 @@ def listAllCountryPaths(startCountry, endCountry):
 			price = sum(list(map(int, path[1::2])))
 			print("Trip price: %d" % price)
 
-def findCountryPathRecursion(startCountry, endCountry, matches, path = []):
+def findCountryPathRecursion(startCountry, endCountry, path = []):
 	path = path + [startCountry]
 	if startCountry == endCountry:
 		return [path]
-
-	allStartCountries = wks.col_values(1) # looks at the 'Start' column
+	allStartCountries = wks['Start Country']
 	indices = [i for i, x in enumerate(allStartCountries) if x == startCountry]
-
 	if len(indices) == 0:
 		return []
 	paths = []
 	for index in indices:
-		nextCountry = wks.cell(index + 1, 2).value # looks at the 'End' column for that row
+		nextCountry = wks['End Country'][index]
 		if nextCountry not in path:
-			price = wks.cell(index + 1, 3).value 
-			newpaths = findCountryPathRecursion(nextCountry, endCountry, matches, path + [price])
+			print(nextCountry)
+			print(path)
+			#price = wks[' USD equiv avg '][index]
+			#nationality = wks['Nationality of Migrant'][index]
+			#service = wks['Service'][index]
+			#tripData = [price, nationality, service]
+			newpaths = findCountryPathRecursion(nextCountry, endCountry, path)
 			for newpath in newpaths:
 				paths.append(newpath)
 	return paths
 
 
-
-listAllCountryPaths('Niger', 'Italy')
-
-
-
+print(listAllCountryPaths('Syria', 'Germany'))
