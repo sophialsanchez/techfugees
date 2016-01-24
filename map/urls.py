77f382a202306c5ca818f842fea586b8bb53461b
Sophia Sanchez <sophiasanchez@sparkcentral.com>
2016-01-23 21:43:04 -0800

    fixed bug with N'Djamena city lookup failureby adding ' to url regex

--- map/urls.py
+++ map/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/queryByStartCity/(?P<slug1>[-\w\d]+)/(?P<slug2>[-\w\d]+)$', views.queryByStartCity, name='queryByStartCity'),
+    url(r'^/queryByStartCity/(?P<slug1>[-\w\d]+)/(?P<slug2>[-\w\d\']+)$', views.queryByStartCity, name='queryByStartCity'),
     url(r'^/getCitiesInACountry/(?P<slug>[-\w\d]+)$', views.getCitiesInACountry, name='getCitiesInACountry'),
 ]
